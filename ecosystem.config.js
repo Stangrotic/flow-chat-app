6e18efeaa8faf96e7411d222952d9799f2aa6ab9
Stephen King <stephen@stephenking.io>
2023-06-13 08:13:43 -0600

    Flow Second Push

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,16 @@
 module.exports = {
-    apps: [
-      {
-        name: 'my-app',
-        script: 'npm',
-        args: 'start',
-        cwd: '/Users/stephenking/Desktop/flow-chat-app/', // replace this with the actual path
+  apps: [
+    {
+      name: 'my-app',
+      script: 'serve',
+      args: '-s build -l 3000',
+      env: {
+        "PM2_SERVE_PATH": "./build",
+        "PM2_SERVE_PORT": 3000,
+        "PM2_SERVE_SPA": "true",
+        "PM2_SERVE_HOMEPAGE": "/index.html"
       },
-    ],
-  };
-  
-  
+      cwd: '/Users/stephenking/Desktop/flow-chat-app/', // replace this with the actual path
+    },
+  ],
+};
